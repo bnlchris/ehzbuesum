3de8c5729683c9f5233332bd451b9904e55d98f4
Christian Schultz <bnlchris@web.de>
2019-01-23 13:18:12 +0100

    Try 4

--- js/app.js
+++ js/app.js
@@ -209,7 +209,7 @@ function toggleSize() {
 // Register the Service Worker after loading the first page
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
+    navigator.serviceWorker.register('/ehzbuesum/sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
