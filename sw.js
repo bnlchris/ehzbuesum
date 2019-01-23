dfe97f703e4d1e652b9d5ea2324f984ad21c0dfc
Christian Schultz <bnlchris@web.de>
2019-01-23 13:31:40 +0100

    Try 6

--- sw.js
+++ sw.js
@@ -1,8 +1,17 @@
 // Install Service Worker
 self.addEventListener('install', function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open('static-v1').then(function(cache) {
-      return cache.addAll([
+    caches.open('myCache')
+      .then(function(cache) {
+        console.log('Caching ...', urlsToCache);
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Array for files to be cached
+let urlsToCache = [
         '/ehzbuesum/index.html',
         '/ehzbuesum/anfahrt.html',
         '/ehzbuesum/buchen.html',
@@ -35,77 +44,40 @@ self.addEventListener('install', function(event) {
         '/ehzbuesum/img/10.jpg',
         '/ehzbuesum/js/app.js',
         '/ehzbuesum/js/data.js'
-      ]);
-    })
-  );
-});
+]
 
 // Service Worker should return requests with cache, otherwise fetch data from network
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || event.default();
-    }).catch(function() {
-      return caches.match('index.html');
-    })
-  );
-});
-
-// Update Service Worker
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('static-v2').then(function(cache) {
-      return cache.addAll([
-        '/ehzbuesum/index.html',
-        '/ehzbuesum/anfahrt.html',
-        '/ehzbuesum/buchen.html',
-        '/ehzbuesum/datenschutz.html',
-        '/ehzbuesum/eindruecke.html',
-        '/ehzbuesum/impressum.html',
-        '/ehzbuesum/preise.html',
-        '/ehzbuesum/css/styles.css',
-        '/ehzbuesum/img/anfahrt_desktop.jpg',
-        '/ehzbuesum/img/anfahrt_tablet.jpg',
-        '/ehzbuesum/img/anfahrt_mobil.jpg',
-        '/ehzbuesum/img/datenschutz_desktop.jpg',
-        '/ehzbuesum/img/datenschtz_tablet.jpg',
-        '/ehzbuesum/img/datenschutz_mobil.jpg',
-        '/ehzbuesum/img/einzel_desktop.jpg',
-        '/ehzbuesum/img/einzel_tablet.jpg',
-        '/ehzbuesum/img/einzel_mobil.jpg',
-        '/ehzbuesum/img/fruehstueck_desktop.jpg',
-        '/ehzbuesum/img/fruehstueck_tablet.jpg',
-        '/ehzbuesum/img/fruehstueck_mobil.jpg',
-        '/ehzbuesum/img/impressum_desktop.jpg',
-        '/ehzbuesum/img/impressum_tablet.jpg',
-        '/ehzbuesum/img/impressum_mobil.jpg',
-        '/ehzbuesum/img/preise_desktop.jpg',
-        '/ehzbuesum/img/preise_tablet.jpg',
-        '/ehzbuesum/img/preise_mobil.jpg',
-        '/ehzbuesum/img/willkommen_desktop.jpg',
-        '/ehzbuesum/img/willkommen_tablet.jpg',
-        '/ehzbuesum/img/willkommen_mobil.jpg',
-        '/ehzbuesum/img/10.jpg',
-        '/ehzbuesum/js/app.js',
-        '/ehzbuesum/js/data.js'
-      ]);
-    })
-  );
-});
+    // prevent default fetch event
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // if request URL in cache, use it
+                if (response) {
+                    console.log('URL used from cache')
+                    return response;
+                }
+                // if not, fetch from network
+                console.log('Network request');
+                return fetch(event.request); 
+            })
+        )
+})
 
 // Check for old Service Workers and delete them
 self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['static-v2'];
+
+  const cacheWhitelist = ['myCache'];
 
   event.waitUntil(
-    caches.keys(function(cacheNames) {
+    caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) == -1) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
         })
-      )
+      );
     })
   );
 });
