af4420a7f8b48a436cdd678f7535403f3c5d2fec
Christian Schultz <bnlchris@web.de>
2019-01-23 13:49:20 +0100

    Try 8

--- sw.js
+++ sw.js
@@ -20,27 +20,6 @@ let urlsToCache = [
         '/ehzbuesum/impressum.html',
         '/ehzbuesum/preise.html',
         '/ehzbuesum/css/styles.css',
-        '/ehzbuesum/img/anfahrt_desktop.jpg',
-        '/ehzbuesum/img/anfahrt_tablet.jpg',
-        '/ehzbuesum/img/anfahrt_mobil.jpg',
-        '/ehzbuesum/img/datenschutz_desktop.jpg',
-        '/ehzbuesum/img/datenschtz_tablet.jpg',
-        '/ehzbuesum/img/datenschutz_mobil.jpg',
-        '/ehzbuesum/img/einzel_desktop.jpg',
-        '/ehzbuesum/img/einzel_tablet.jpg',
-        '/ehzbuesum/img/einzel_mobil.jpg',
-        '/ehzbuesum/img/fruehstueck_desktop.jpg',
-        '/ehzbuesum/img/fruehstueck_tablet.jpg',
-        '/ehzbuesum/img/fruehstueck_mobil.jpg',
-        '/ehzbuesum/img/impressum_desktop.jpg',
-        '/ehzbuesum/img/impressum_tablet.jpg',
-        '/ehzbuesum/img/impressum_mobil.jpg',
-        '/ehzbuesum/img/preise_desktop.jpg',
-        '/ehzbuesum/img/preise_tablet.jpg',
-        '/ehzbuesum/img/preise_mobil.jpg',
-        '/ehzbuesum/img/willkommen_desktop.jpg',
-        '/ehzbuesum/img/willkommen_tablet.jpg',
-        '/ehzbuesum/img/willkommen_mobil.jpg',
         '/ehzbuesum/js/app.js',
         '/ehzbuesum/js/data.js'
 ]
@@ -80,3 +59,39 @@ self.addEventListener('activate', function(event) {
     })
   );
 });
+
+/*
+let urlsToCache = [
+        '/ehzbuesum/index.html',
+        '/ehzbuesum/anfahrt.html',
+        '/ehzbuesum/buchen.html',
+        '/ehzbuesum/datenschutz.html',
+        '/ehzbuesum/eindruecke.html',
+        '/ehzbuesum/impressum.html',
+        '/ehzbuesum/preise.html',
+        '/ehzbuesum/css/styles.css',
+        '/ehzbuesum/img/anfahrt_desktop.jpg',
+        '/ehzbuesum/img/anfahrt_tablet.jpg',
+        '/ehzbuesum/img/anfahrt_mobil.jpg',
+        '/ehzbuesum/img/datenschutz_desktop.jpg',
+        '/ehzbuesum/img/datenschtz_tablet.jpg',
+        '/ehzbuesum/img/datenschutz_mobil.jpg',
+        '/ehzbuesum/img/einzel_desktop.jpg',
+        '/ehzbuesum/img/einzel_tablet.jpg',
+        '/ehzbuesum/img/einzel_mobil.jpg',
+        '/ehzbuesum/img/fruehstueck_desktop.jpg',
+        '/ehzbuesum/img/fruehstueck_tablet.jpg',
+        '/ehzbuesum/img/fruehstueck_mobil.jpg',
+        '/ehzbuesum/img/impressum_desktop.jpg',
+        '/ehzbuesum/img/impressum_tablet.jpg',
+        '/ehzbuesum/img/impressum_mobil.jpg',
+        '/ehzbuesum/img/preise_desktop.jpg',
+        '/ehzbuesum/img/preise_tablet.jpg',
+        '/ehzbuesum/img/preise_mobil.jpg',
+        '/ehzbuesum/img/willkommen_desktop.jpg',
+        '/ehzbuesum/img/willkommen_tablet.jpg',
+        '/ehzbuesum/img/willkommen_mobil.jpg',
+        '/ehzbuesum/js/app.js',
+        '/ehzbuesum/js/data.js'
+]
+*/
